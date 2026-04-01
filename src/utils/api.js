0dc9b4cd7bf2fc5fcf1f8106c5c9656da0f71c14
avdrxxa <damian.raluka.andreea23@gmail.com>
2026-04-01 16:46:31 +0200

    java script on place only small changes fix and event info now done yet css or javascript

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 
 export function getBaseUrl() {
   if (window.location.hostname.includes("localhost")) {
-    return "http://localhost:3000/";
+    return "http://localhost:3000/api/";
   }
   // TODO: Add deployed backend URL
-  return "https://your-backend.vercel.app/";
+  return "https://webbshop-2026-ply7firm6-ludvig-ds-projects.vercel.app/api/";
 }
